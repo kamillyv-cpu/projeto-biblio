785a295c7e40dfc26dbb78fd2de1c694f309634b
Kamilly Vitória de Freitas Saldanha <20231094010032@ifrn.local>
2026-08-27 10:16:34 -0300

    entidade e admin

--- acervo/admin.py
+++ acervo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Livro, Autor
 
-# Register your models here.
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_lancamento', 'resumo', 'nome_editora')
+    search_fields = ('titulo',)
+
+admin.site.register(Livro, LivroAdmin)
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'ano_nascimento')
+    search_fields = ('nome',)
+
+admin.site.register(Autor,AutorAdmin)
--- acervo/models.py
+++ acervo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Livro(models.Model):
+    titulo = models.CharField(max_length=100)
+    data_lancamento = models.IntegerField()
+    resumo = models.CharField(max_length=100)
+    nome_editora = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.titulo
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    ano_nascimento = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
